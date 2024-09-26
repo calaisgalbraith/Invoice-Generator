6ec6f7170d21ce6893ee8958653aaee9cd7ec007
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-09-26 17:05:26 -0500

    added header & container

---
+++ invoice-generator/src/components/InvoiceContainer/InvoiceContainer.js
@@ -0,0 +1,16 @@
+import './InvoiceContainer.css';
+// import InputContainer from '../InvoiceFields/InputContainer/InputContainer';
+
+function InvoiceContainer() {
+  return (
+    <>
+        <div className="backgroundContainer"></div>
+        <div className="invoiceContainer container-fluid">
+          {/* <InputContainer />
+          <InputContainer /> */}
+        </div>
+    </>
+  );
+}
+
+export default InvoiceContainer;
---
+++ invoice-generator/src/components/InvoiceHeader/InvoiceHeader.js
@@ -0,0 +1,14 @@
+import './InvoiceHeader.css';
+
+function InvoiceHeader() {
+  return (
+    <header>
+        <div className="headerContainer">
+            <h1> Invoice Generator</h1>
+            <p>Create & download a PDF of your invoice!</p>
+        </div>
+    </header>
+  );
+}
+
+export default InvoiceHeader;
